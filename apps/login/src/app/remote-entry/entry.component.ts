9aaefce4066105d4e44504c613ebc112cdbf9f22
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-04-08 15:16:42 +0200

    set up login app

--- apps/login/src/app/remote-entry/entry.component.ts
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '@ng-mf/data-access-user';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NxWelcomeComponent],
+  imports: [CommonModule, FormsModule],
   selector: 'ng-mf-login-entry',
-  template: `<ng-mf-nx-welcome></ng-mf-nx-welcome>`,
+  template: `
+    <div class="login-app">
+      <form class="login-form" (ngSubmit)="login()">
+        <label>
+          Username:
+          <input type="text" name="username" [(ngModel)]="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" [(ngModel)]="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <div *ngIf="isLoggedIn$ | async">User is logged in!</div>
+    </div>
+  `,
+  styles: [
+    `
+      .login-app {
+        width: 30vw;
+        border: 2px dashed black;
+        padding: 8px;
+        margin: 0 auto;
+      }
+      .login-form {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin: 0 auto;
+        padding: 8px;
+      }
+      label {
+        display: block;
+      }
+    `,
+  ],
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  username = '';
+  password = '';
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  constructor(private userService: UserService) {}
+
+  login() {
+    this.userService.checkCredentials(this.username, this.password);
+  }
+}
